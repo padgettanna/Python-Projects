7c368a75c0eb7d20c783f2bbf5cd496d8bb63918
Anna Padgett <119985556+padgettanna@users.noreply.github.com>
2022-12-29 17:50:21 -0800

    Create Blind Auction.py

---
+++ Blind Auction.py
@@ -0,0 +1,17 @@
+from replit import clear
+from art import logo
+print(logo)
+all_bidders = {}
+stop_bidding = False
+while not stop_bidding:
+  name = input("What is your name? ")
+  bid = input("What's your bid? $")
+  all_bidders[name] = bid
+  other_bidders = input("Are there any other bidders? Type 'yes' or 'no'. \n")
+  clear()
+  if other_bidders == 'no':
+    stop_bidding = True
+    for highest_bidder in all_bidders:
+      highest_bidder = max(all_bidders, key = all_bidders.get)
+      highest_bid = all_bidders[highest_bidder]
+    print(f"The winner is {highest_bidder} with a bid of ${highest_bid}")
