eb05080b18ebecb0590dd99028d3c48f6c8bb2ab
Anna Padgett <119985556+padgettanna@users.noreply.github.com>
2023-01-16 11:23:13 -0800

    Add files via upload

---
+++ Quiz Game/data.py
@@ -0,0 +1,82 @@
+question_data = [
+    {
+        "category": "Animals",
+        "type": "boolean",
+        "difficulty": "easy",
+        "question": "A slug's blood is green.",
+        "correct_answer": "True",
+        "incorrect_answers": ["False"]
+    },
+    {
+        "category": "General Knowledge",
+        "type": "boolean",
+        "difficulty": "easy",
+        "question": "A scientific study on peanuts in bars found traces of over 100 unique specimens of urine.",
+        "correct_answer": "False",
+        "incorrect_answers": ["True"]
+    },
+    {
+        "category": "General Knowledge",
+        "type": "boolean",
+        "difficulty": "easy",
+        "question": "The color orange is named after the fruit.",
+        "correct_answer": "True",
+        "incorrect_answers": ["False"]
+    },
+    {
+        "category": "History",
+        "type": "boolean",
+        "difficulty": "easy",
+        "question": "United States President John F. Kennedy was assassinated during his presidential "
+                    "motorcade in Atlanta, Georgia on November 22nd, 1963.",
+        "correct_answer": "False",
+        "incorrect_answers": ["True"]
+    },
+    {
+        "category": "Entertainment: Video Games",
+        "type": "boolean",
+        "difficulty": "easy",
+        "question": "Minecraft can be played with a virtual reality headset.",
+        "correct_answer": "True",
+        "incorrect_answers": ["False"]
+    },
+    {
+        "category": "Science & Nature",
+        "type": "boolean",
+        "difficulty": "easy",
+        "question": "An exothermic reaction is a chemical reaction that releases energy by radiating electricity.",
+        "correct_answer": "False",
+        "incorrect_answers": ["True"]
+    },
+    {
+        "category": "Entertainment: Music",
+        "type": "boolean",
+        "difficulty": "easy",
+        "question": "Daft Punk originated in France.",
+        "correct_answer": "True",
+        "incorrect_answers": ["False"]
+    },
+    {
+        "category": "History",
+        "type": "boolean",
+        "difficulty": "easy",
+        "question": "Kublai Khan is the grandchild of Genghis Khan?",
+        "correct_answer": "True",
+        "incorrect_answers": ["False"]
+    },
+    {
+        "category": "Entertainment: Music",
+        "type": "boolean",
+        "difficulty": "easy",
+        "question": "Lead Singer Rivers Cuomo of American rock band Weezer attended Harvard.",
+        "correct_answer": "True", "incorrect_answers": ["False"]
+    },
+    {
+        "category": "General Knowledge",
+        "type": "boolean",
+        "difficulty": "easy",
+        "question": "Ping-Pong originated in England",
+        "correct_answer": "True",
+        "incorrect_answers": ["False"]
+    }
+]
