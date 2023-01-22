e3139ad72b10bfa73b3c4993d1e0c4b909d38cae
Anna Padgett <119985556+padgettanna@users.noreply.github.com>
2023-01-22 11:40:32 -0800

    Add files via upload

---
+++ PongGame/paddle.py
@@ -0,0 +1,26 @@
+from turtle import Turtle
+
+
+class Paddle(Turtle):
+
+    def __init__(self, position):
+        super().__init__()
+        self.speed("fastest")
+        self.penup()
+        self.goto(position)
+        self.color("white")
+        self.shape("square")
+        self.shapesize(stretch_wid=5, stretch_len=1)
+
+    def up(self):
+        new_y = self.ycor() + 20
+        if self.ycor() < 240:
+            self.goto(self.xcor(), new_y)
+
+    def down(self):
+        new_y = self.ycor() - 20
+        if self.ycor() > -240:
+            self.goto(self.xcor(), new_y)
+
+
+
